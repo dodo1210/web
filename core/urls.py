b9d0653accc87d68141777c01db88aed84edf2cc
tomaz <tomazrvb@hotmail.com>
2017-05-25 19:26:52 -0300

    Novo

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.login_user, name='login_user'),
+    url(r'^login/$', views.login, name='login_user'),
     #url(r'^login/$', views.login, {'template_name': 'index.html'}, name='login')
 ] 
--- core/views.py
+++ core/views.py
@@ -1,22 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
+
 # Create your views here.
-from django.http import HttpResponse
-from django.contrib.auth.models import User
+from django.http import HttpResponse,HttpResponseRedirect
+
+from django.contrib import auth
+from django.template.context_processors import csrf
 
 def index(request):
 	return render(request,"index.html")
 
-def login_user(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('senha')     
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect('/')
-            else:
-                return render(request, 'index.html', {'error_message': 'Your account has been disabled'})
-        else:
-            return render(request, 'index.html', {'error_message': 'Invalid login'})
-    return render(request, 'index.html')
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response("login.html",c)
+
+def auth_view(request):
+    username = request.POST.get('username','')
+    password = request.POST.get('senha','')
+    user  = auth.authenticate(username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/home')
+    else:
+        return render(request, 'index.html', {'error_message': 'Invalid login'})
+
+
+
+
+
+    """if request.method == "POST":
+                    username = request.POST.get('username')
+                    password = request.POST.get('senha')     
+                    user = authenticate(username=username, password=password)
+                    if user is not None:
+                        if user.is_active:
+                            login(request, user)
+                            return redirect('/')
+                        else:
+                            return render(request, 'index.html', {'error_message': 'Your account has been disabled'})
+                    else:
+                        return render(request, 'index.html', {'error_message': 'Invalid login'})
+                return render(request, 'index.html')
+            """
